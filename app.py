b5b71b1c4463f20942889417d7af308fa1d089a6
jonathan-pp <84352539+jonathan-pp@users.noreply.github.com>
2026-05-11 19:26:22 +0200

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello lagonna 3.6 - test2  EPSIC - DevOps - ALED"
+    return "Hello lagonna 3.6 - avec variable d'enviroment  EPSIC - DevOps - ALED"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)

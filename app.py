3918b8276eef53e2eac9203069999cfc47b7758b
jonathan-pp <84352539+jonathan-pp@users.noreply.github.com>
2026-05-11 21:35:50 +0200

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "test Hello EPSIC 158! - Modifié automatiquement par Jenkins ! "
+    return "test83298r23089jfi32jm4iognmiog9023tjnm Hello EPSIC 158! - Modifié automatiquement par Jenkins ! "
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
